11c295e35dd8eb75b726a9f3ed0df37b5dde91c9
Harif3562 <ricardosamayoa966@gmail.com>
2026-08-06 00:01:14 -0600

    prueba de conexion

--- ferreteria-backend/db.js
+++ ferreteria-backend/db.js
@@ -2,15 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Requerido para conectar Render con Neon
+  }
 });
 
 pool.on('connect', () => {
-  console.log('✅ Conectado a la base de datos PostgreSQL');
+  console.log(' Conectado a la base de datos PostgreSQL');
 });
 
 module.exports = pool;
